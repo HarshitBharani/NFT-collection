6ce4871db9fbd12d405138da3ce0b3347392049f
Harshit Bharani <50341476+HarshitBharani@users.noreply.github.com>
2022-06-01 11:29:17 +0530

    intial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,3 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x484526D1affa90012357C778e1A65Bb4Faa9Add5";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
